661f0a8d64ef95db081e2683f62758bbcf5cff99
EggheadChienNB <larry526kimo@hotmail.com>
2016-07-31 23:53:09 +0800

    npm run dev is workable now at port 9527, and it can refresh itself automatically !!

--- app/main.js
+++ app/main.js
@@ -1,5 +1,8 @@
 'use strict';
-var component = require('./component.js');
+// 第四、五行意思是一樣的 只是module的不同:一個是用ES6 一個是用CommonJS
+// https://fakefish.github.io/react-webpack-cookbook/Requiring-files.html
+// import component from './component.js'; // ES6
+var component = require('./component.js'); // CommonJS
 
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,11 @@
 
 
 module.exports = {
-    entry: path.resolve(__dirname, 'app/main.js'),
+    entry: [
+		'webpack/hot/dev-server',
+		'webpack-dev-server/client?http://localhost:9527',
+		path.resolve(__dirname, 'app/main.js')
+	],
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
