26658832f44b0e9118b9c819e87ab8ef5e77ace3
Larry Chien <larry.chien@micromacro.com.tw>
2016-08-02 17:06:51 +0800

    1.react and react-dom is belong to dependencies, not devDependencies 2.build a React simple sample

--- app/main.js
+++ app/main.js
@@ -1,8 +1,17 @@
-'use strict';
+// 'use strict';
 // 第四、五行意思是一樣的 只是module的不同:一個是用ES6 一個是用CommonJS
 // https://fakefish.github.io/react-webpack-cookbook/Requiring-files.html
 // import component from './component.js'; // ES6
-var component = require('./component.js'); // CommonJS
+// var component = require('./component.js'); // CommonJS
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './component.jsx';
 
+main();
 
-document.body.appendChild(component());
+function main() {
+	ReactDOM.render(
+	<div>
+		<Hello />
+	</div>, document.getElementById('app'));
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,42 @@
 ﻿var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+var pathToReactDOM = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
 
 
-module.exports = {
+var config = {
     entry: [
+		path.resolve(__dirname, 'app/main.js'),
 		'webpack/hot/dev-server',
 		'webpack-dev-server/client?http://localhost:9527',
-		path.resolve(__dirname, 'app/main.js')
 	],
+	resolve: {
+        alias: {
+			'react': pathToReact,
+			'react-dom': pathToReactDOM
+        }
+    },
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
     },
+	module: {
+		loaders: [{
+			test: /\.jsx?$/, // 用正則來匹配路徑，這段意思是匹配 js 或者 jsx
+			exclude: /node_modules/,
+			loader: 'babel-loader' // 加載模組 "babel-loader"
+		}, {
+			test: /\.css$/, // Only .css files
+			loader: 'style!css' // Run both loaders
+		}, {
+			test: /\.scss$/,
+			loader: 'style!css!sass'
+		}, {
+			test: /\.(png|jpg)$/,
+			loader: 'url?limit=25000' //25kb以下的圖檔會轉成base64,大於的則會透過file-loader這個package載入
+		}],
+		noParse: [pathToReact]
+	}
 };
+
+module.exports = config;
